48ecf41e444bdc62521c62f5f68fba17bd2be813
Curtis Thompson <curtiswaynethompson@gmail.com>
2023-08-31 17:17:02 +0700

    Fix KML Document tag

--- build_maps.py
+++ build_maps.py
@@ -16,11 +16,13 @@ def dataframe_to_kml(df, name_col, desc_col, lat_col, lon_col):
     """Convert a DataFrame into a KML file."""
     text = '<?xml version="1.0" encoding="UTF-8"?>\n'
     text += '<kml xmlns="http://www.opengis.net/kml/2.2">\n'
+    text += '<Document>\n'
     for index, row in df.iterrows():
         text += generate_placemark_kml(row[name_col],
                                        row[desc_col],
                                        row[lat_col],
                                        row[lon_col])
+    text += '</Document>\n'
     text += '</kml>'
     return text
 
@@ -35,10 +37,16 @@ def hills_csv_to_kml():
     with open('./data/hills_marilyn.kml', 'w') as f:
         f.write(kml)
     
-    # Create Marilyn KML
+    # Create Simm KML
     df_s = df.loc[df.Sim == 1]
     kml = dataframe_to_kml(df_s, 'Name', 'KMLDesc', 'Latitude', 'Longitude')
     with open('./data/hills_simm.kml', 'w') as f:
+        f.write(kml)
+    
+    # Create Simm KML
+    df_se = df.loc[(df.Sim == 1) & (df.Country == 'England')]
+    kml = dataframe_to_kml(df_se, 'Name', 'KMLDesc', 'Latitude', 'Longitude')
+    with open('./data/hills_simm_england.kml', 'w') as f:
         f.write(kml)
 
 
